f97b994fe2d705eef15c89936530127fb183dd83
Gnorshk <leggojohn.js@gmail.com>
2020-08-04 19:21:46 +1000

    simple real-time updating of the right half of the screen

--- my_test_website.py
+++ my_test_website.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import RadioField
 from wtforms.validators import DataRequired
