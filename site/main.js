1fb2b813e22119b09de0bd7b473c57ec08478cda
Gnorshk <leggojohn.js@gmail.com>
2020-08-09 11:54:49 +1000

    added coloured dichotomy labels to the first row and column

--- site/main.js
+++ site/main.js
@@ -89,10 +89,11 @@ function showResults(){
     const cols = [[0, 4, 8, 12], [1, 5, 9, 13], [2, 6, 10, 14], [3, 7, 11, 15]];
     const rows = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]];
     
-    
-    //todo: make the list of lists of dichotomies the same by inverting the 1s and zeros on list-row-dichs
-    //and swapping around the last two lists on list_col_dichs so that they both are [[1, 0, 1], [1, 1, 0], [0, 1, 1], [0, 0, 0]]
-    if (!(findInArray(2, list_col_dichs))) {    //THESE ARE NOT WORKING FIX ASAP   
+    /*i could have make the list of lists of dichotomies the same by inverting the 1s and zeros on list-row-dichs
+    and swapping around the last two lists on list_col_dichs so that they both are ["101", "110", "011", "000"]
+    however, i didn't because i wanted to preserve the 16personalities type-grid layout*/
+
+    if (!(findInArray(2, list_col_dichs))) {  
         if (!(findInArray(list_col_dichs.join(''), ['101', '110', '000', '011'])) && list_unsure_answers[0] !== 3) {
             list_col_dichs[list_unsure_answers[0]] = Math.abs(list_col_dichs[list_unsure_answers[0]] - 1);
         }
@@ -120,42 +121,117 @@ function showResults(){
         cols[i].forEach(j => tally_list[j] += col_chances[i]);
         rows[i].forEach(j => tally_list[j] += row_chances[i]);
     }
-    
+
+    // these are the dichotomies that will be displayed in the table.
+    final_dichs = [];
+    for (i = 0; i < 3; i++) {
+        if (list_col_dichs[i] === 2) {
+            final_dichs.push('<span style="color:red">0</span>');
+            final_dichs.push('<span style="color:red">0</span>');
+        } else if (list_col_dichs[i] === 0) {
+            final_dichs.push(`<span style="color:limegreen">1</span>`);
+            final_dichs.push(`<span style="color:red">0</span>`);
+        } else {
+            final_dichs.push(`<span style="color:red">0</span>`);
+            final_dichs.push(`<span style="color:limegreen">1</span>`);
+        }
+    }
+    for (i = 0; i < 3; i++) {
+        if (list_row_dichs[i] === 2) {
+            final_dichs.push('<span style="color:red">0</span>');
+            final_dichs.push('<span style="color:red">0</span>');
+        } else if (list_row_dichs[i] === 0) {
+            final_dichs.push(`<span style="color:limegreen">1</span>`);
+            final_dichs.push(`<span style="color:red">0</span>`);
+        } else {
+            final_dichs.push(`<span style="color:red">0</span>`);
+            final_dichs.push(`<span style="color:limegreen">1</span>`);
+        }
+    }
+    console.log(final_dichs);
+    /*
+    0: initiating 1:responding
+    2: direct 3: informative
+    4: control 5: movement
+    6: abstract 7: concrete
+    8: affiliative 9: pragmatic
+    10: systematic 11: interest
+    */
     resultsContainer.innerHTML = `<table>
         <tr>
-            <th>Results:</th>
-            <th>Finisher</th>
-            <th>Background</th>
-            <th>In-charge</th>
-            <th>Starter</th>
+            <th>Results</th>
+            <th>
+                Finisher
+                Responding:${final_dichs[1]}
+                Direct:${final_dichs[2]}
+                Movement:${final_dichs[5]}
+            </th>
+            <th>
+                Background
+                Responding:${final_dichs[1]}
+                Informative:${final_dichs[3]}
+                Control:${final_dichs[4]}
+            </th>
+            <th>
+                In-charge
+                Initiating:${final_dichs[0]}
+                Direct:${final_dichs[2]}
+                Control:${final_dichs[4]}
+            </th>
+            <th>
+                Starter
+                Initiating:${final_dichs[0]}
+                Informative:${final_dichs[3]}
+                Movement:${final_dichs[5]}
+            </th>
         </tr>
         <tr>
-            <th>Intellectual</th>
-            <td>INTJ: ${tally_list[0]}</td>
-            <td>INTP: ${tally_list[1]}</td>
-            <td>ENTJ: ${tally_list[2]}</td>
-            <td>ENTP: ${tally_list[3]}</td>
+            <th>
+                Intellectual
+                Abstract:${final_dichs[6]}
+                Pragmatic:${final_dichs[9]}
+                Systematic:${final_dichs[10]}
+            </th>
+            <td>INTJ:${tally_list[0]}</td>
+            <td>INTP:${tally_list[1]}</td>
+            <td>ENTJ:${tally_list[2]}</td>
+            <td>ENTP:${tally_list[3]}</td>
         </tr>
         <tr>
-            <th>Idealist</th>
-            <td>INFJ: ${tally_list[4]}</td>
-            <td>INFP: ${tally_list[5]}</td>
-            <td>ENFJ: ${tally_list[6]}</td>
-            <td>ENFP: ${tally_list[7]}</td>
+            <th>
+                Idealist
+                Abstract:${final_dichs[6]}
+                Affiliative:${final_dichs[8]}
+                Interest:${final_dichs[11]}
+            </th>
+            <td>INFJ:${tally_list[4]}</td>
+            <td>INFP:${tally_list[5]}</td>
+            <td>ENFJ:${tally_list[6]}</td>
+            <td>ENFP:${tally_list[7]}</td>
         </tr>
         <tr>
-            <th>Guardian</th>
-            <td>ISTJ: ${tally_list[8]}</td>
-            <td>ISFJ: ${tally_list[9]}</td>
-            <td>ESTJ: ${tally_list[10]}</td>
-            <td>ESFJ: ${tally_list[11]}</td>
+            <th>
+                Guardian
+                Concrete:${final_dichs[7]}
+                Affiliative:${final_dichs[8]}
+                Systematic:${final_dichs[10]}
+            </th>
+            <td>ISTJ:${tally_list[8]}</td>
+            <td>ISFJ:${tally_list[9]}</td>
+            <td>ESTJ:${tally_list[10]}</td>
+            <td>ESFJ:${tally_list[11]}</td>
         </tr>
         <tr>
-            <th>Artisan</th>
-            <td>ISTP: ${tally_list[12]}</td>
-            <td>ISFP: ${tally_list[13]}</td>
-            <td>ESTP: ${tally_list[14]}</td>
-            <td>ESFP: ${tally_list[15]}</td>
+            <th>
+                Artisan
+                Concrete:${final_dichs[7]}
+                Pragmatic:${final_dichs[9]}
+                Interest:${final_dichs[11]}
+            </th>
+            <td>ISTP:${tally_list[12]}</td>
+            <td>ISFP:${tally_list[13]}</td>
+            <td>ESTP:${tally_list[14]}</td>
+            <td>ESFP:${tally_list[15]}</td>
         </tr>
     </table>`;
 }
