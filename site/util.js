b34d0cbc9d51efb41899df09ea3f56bf0668bc20
Gnorshk <leggojohn.js@gmail.com>
2020-08-10 15:02:46 +1000

    Revert "changed to es6 import format"
    This reverts commit 2eddfe2f925d0af1ef0a5bf22eb94c002f278f25.

--- site/util.js
+++ site/util.js
@@ -1,25 +1,3 @@
-export function get_colrow_chance(list, a, b, c) {
-    let output = 0;
-    if (list[0] == a && list[1] == b) {
-        output += 1;
-    }
-    if (list[1] == b && list[2] == c) {
-        output += 1;
-    }
-    if (list[0] == a && list[2] == c) {
-        output += 1;
-    }
-    return output;
-}
 
-export function findInArray(value, array) {
-    let output = false;
-    for (var i = 0; i < array.length; i++){
-        const item = array[i]
-        if (item === value) {
-            output = true;
-            break;
-        }
-    }
-    return output;
-}
+
+//this file will have multiple exports
