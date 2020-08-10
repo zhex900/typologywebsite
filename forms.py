b471b498519dcddfe952441d650d7855d951196e
Gnorshk <leggojohn.js@gmail.com>
2020-08-10 14:17:13 +1000

    reorganised and renamed files for deployment

--- forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import RadioField
-from wtforms.validators import DataRequired
-
-class DichotomyForm(FlaskForm):
-    dichotomies = [("a", "a"), ("b", "b")]
-    dichotomy = RadioField("yes", choices=dichotomies)
